d19fe370e5136840c1f2a93a2f075b0b77518173
Yushu Hsiao <yushuhsiao@gmail.com>
2025-05-08 09:59:41 +0800

    Dapper.Contrib.Extensions

---
+++ System.Data/SqlMapperExtensions.Contrib.cs
@@ -0,0 +1,11 @@
+﻿using System.Data;
+using Dapper.Contrib.Extensions;
+
+namespace Dapper.Contrib
+{
+    public static class SqlMapperExtensions
+    {
+        //public static long Insert<T>(this IDbTransaction transaction, T entityToInsert, int? commandTimeout = null)
+        //    => transaction.Connection.Insert<T>(transaction.Connection, entityToInsert, transaction, commandTimeout);
+    }
+}
